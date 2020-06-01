3e07abc7cdb4d66e37226e3bf137b7a8fa7d9eca
Alex Melodiev <alex.melodiev@gmail.com>
2020-06-01 23:10:21 +0500

    =mix error fixing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+const mix  = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +10,6 @@ const { mix } = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+п
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
